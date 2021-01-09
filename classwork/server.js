37bf6e8e96882671b3cc4644320e0cf4640e2db3
Tanjir16 <tanjirahmedemon@gmail.com>
2021-01-09 23:00:29 +0600

    version of Node

--- classwork/server.js
+++ classwork/server.js
@@ -1,103 +1,106 @@
 var http = require('http')
 var express = require('express')
-var bodyParser = require('body-parser')
-var app = express()
-var server = http.Server(app)
-var Article = require('./article.model')
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-//DB Connection 
-
-var mongoose = require('mongoose')
-mongoose.Promise = global.Promise
-var dbURL = 'mongodb://localhost:27017/cw10' //change this if you are using Atlas
-mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.connection.on('error', function (err) {
+var bodyParser = require('body-parser') 
+var app = express() 
+var server = http.Server(app) 
+var Article = require('./article.model')  
+//mongose
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+var dbURL = 'mongodb://localhost:27017/cse309'
+mongoose.connect(dbURL, {useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.connection.on('error', function (err)  {
     console.log(err)
-})
+ });
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true})) 
+
+
 
 
 // your server routes go here
-
-app.get('/', function (request, response) {
-    //console.log(request)
-    response.sendFile(__dirname + '/index.html')
+app.get('/', function(request, response){
+    
+    response.sendFile(__dirname + '/index.html');
 })
 
-app.get('/second', function (request, response) {
-    console.log(request)
-    response.sendFile(__dirname + '/second.html')
+app.get('/second', function(request, response){
+    
+    response.sendFile(__dirname + '/second.html');
 })
 
-app.get('/article/form', function (request, response) {
-    //console.log(request)
-    response.sendFile(__dirname + '/form.html')
+app.get('/article/form', function(request, response){
+    // console.log(request);
+    response.sendFile(__dirname + '/form.html');
 })
 
-
-var articles = [{ title: 'test', content: 'test' },
-{ title: 'test2', content: 'test2' },
-{ title: 'test3', content: 'test3' }]
+// var article = [
+//     {    title :"1st title", content :"1st content" },
+//     {    title :"2nd title", content :"2nd content" }];
 
 app.post('/article/new', function (request, response) {
-    var newArticle = new Article(request.body)
-    newArticle.save(function (err, data) {
-        if (err)
-            return response.status(400).json({
-                error: 'Title is missing'
-            })
-        return response.status(200).json({
-            message: 'Article created successfully'
-        })
-    })
-
-
-    // if (newArticle.title) {
-    //     articles.push(newArticle)
-    //     console.log(articles)
-    //     response.json(articles)
-    // }
-    // else {
-    //     response.status(400).json({
-    //         msg: 'no title added'
-    //     })
-    // }
-
+ 
+ var newArticle = new Article(request.body)
+ newArticle.save(function (err, data) {
+   if (err)
+     return response.status(400).json({
+       error: 'Title is missing-> '+data
+     })
+   return response.status(200).json({
+     message: 'Article created successfully -> '+data
+   })
+ })
 })
+
+// app.post('/article/new', function(request, response){
+//     //console.log("serv")
+//     let newArticle = request.body;
+//     if(newArticle.title){
+//         article.push(request.body);
+//         console.log(article);
+//         response.json(article);
+//     }
+//     else{
+//         response.status(400).json({
+//             msg: "No title added"
+//         })
+//     }
+// })
+
+// app.get('/article/:id',function(request, response){
+//     console.log(request.params.id);
+//     response.render('article.ejs',{
+//         article: article[request.params.id]
+//     })
+// })
 
 app.get('/article/:id', function (request, response) {
-
     Article.findById(request.params.id, function (err, data) {
-        response.render('article.ejs', {
-            article: data
-        })
+      response.render('article.ejs', {
+        article: data
+      })
     })
-})
+   })
+   
 
-//     console.log(request.params.id)
-//     response.render('article.ejs', {
-//         article: articles[request.params.id]
-//     })
+// app.get('/articles/all',function(request, response){
+//     response.render('allArticle.ejs',
+//     {article: article}
+//     )
 // })
 
 app.get('/articles/all', function (request, response) {
-
+    console.log("test");
     Article.find({}, function (err, data) {
-        response.render('allArticles.ejs', {
-            articles: data
-        })
+      response.render('allArticle.ejs', {
+        articles: data
+      })
     })
-})
-
-//     response.render('allArticles.ejs', {
-//         articles: articles
-//     })
-// })
+   })
+   
 
 server.listen(process.env.PORT || 3000,
-    process.env.IP || 'localhost', function () {
-        console.log('Server running');
-    })
+process.env.IP || 'localhost', function(){
+console.log('Server running');
+})
 module.exports = {app, server, mongoose}
